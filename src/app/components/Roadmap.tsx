886bbb67d82375a5fec38e0479270d05a1db8a53
Marshall Kurniawan <shrallvierdo@gmail.com>
2024-03-19 11:31:36 +0700

    feat: add roadmap section

--- src/app/components/Roadmap.tsx
+++ src/app/components/Roadmap.tsx
@@ -1,10 +1,102 @@
-import React from "react";
+"use client";
+import Image from "next/image";
+
+import { cn } from "@/lib/utils";
+
+import {
+  Card,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+import { roadmaps } from "@/constant/Roadmap";
 
 function Roadmap() {
   return (
-    <section className="px-8 xl:px-56 2xl:px-128 translate-y-1 relative">
-      <div className="w-[1px] h-24 bg-gradient-to-b from-unit-blue-40 via-unit-purple-20 to-unit-purple-40 from-0% via-90% -translate-x-[1px]" />
-      <div className="size-3 bg-unit-blue-40 rounded-full absolute -translate-x-1.5" />
+    <section className="translate-y-1 flex flex-col items-center gap-y-8">
+      <div className="relative flex flex-col items-center">
+        <div className="w-[1px] h-24 bg-gradient-to-b from-unit-blue-40 via-unit-purple-20 to-unit-purple-40 from-0% via-90%" />
+        <div className="size-3 bg-unit-blue-40 rounded-full absolute bottom-0" />
+      </div>
+      <h2 className="text-4xl font-semibold">
+        2024 Launch{" "}
+        <span className="from-unit-purple-20 to-unit-blue-40 from-0% via-90% bg-gradient-to-r inline-block text-transparent bg-clip-text">
+          Roadmap
+        </span>
+      </h2>
+      <p className="text-xl text-center px-8 xl:px-56 2xl:px-128">
+        By helping cities to tokenize their economy, everyone will have access
+        to have a financial stake in their favorite cities and communities.
+      </p>
+      <div className="flex flex-col w-full items-center">
+        {roadmaps.map((roadmap, index) => (
+          <div
+            key={roadmap.title}
+            className={cn(
+              "flex gap-x-2",
+              index % 2 !== 0 && "flex-row-reverse",
+            )}
+          >
+            <div
+              className={cn(
+                "w-[1px] bg-unit-grey-40",
+                roadmaps.length === index + 1 ? "h-16" : "h-32",
+              )}
+            />
+            {index === 0 && (
+              <div className="size-3 border border-unit-grey-40 bg-unit-grey-10 rounded-full absolute -translate-x-1.5" />
+            )}
+            <div
+              className={cn(
+                "size-3 border border-unit-grey-40 bg-unit-grey-10 rounded-full absolute translate-y-16",
+                index % 2 === 0 ? "-translate-x-1.5" : "translate-x-1.5",
+              )}
+            />
+            <div
+              className={cn(
+                "shadow-lg drop-shadow-sm lg:flex w-fit absolute",
+                index % 2 === 0 ? "translate-x-4" : "-translate-x-4",
+              )}
+            >
+              <div className="relative w-52">
+                <Image
+                  src={roadmap.image}
+                  alt={roadmap.title}
+                  priority={true}
+                  quality={100}
+                  fill
+                  sizes="100vw"
+                  data-loaded="false"
+                  onLoad={(event) => {
+                    event.currentTarget.setAttribute("data-loaded", "true");
+                  }}
+                  className="peer -z-10 rounded-l-md object-cover data-[loaded=false]:hidden"
+                />
+                <div className="absolute -z-10 h-full w-full rounded-l-md bg-black opacity-40 peer-data-[loaded=false]:hidden" />
+                <div className="absolute -z-10 h-full w-full animate-pulse rounded-l-md bg-unit-black-60 peer-data-[loaded=true]:hidden" />
+                <div className="flex h-full w-full flex-col items-center justify-center gap-y-2 px-6 py-8 font-poppins text-white">
+                  <span className="text-2xl font-semibold">{roadmap.date}</span>
+                  <span className="font-medium">{roadmap.month_year}</span>
+                </div>
+              </div>
+              <Card className="flex flex-1 items-center p-0">
+                <CardHeader className="h-full px-6 py-3 justify-between">
+                  <CardTitle className="text-2xl flex flex-col">
+                    <span className="text-sm text-unit-grey-40 font-normal tracking-wide">
+                      UNIT CITIES
+                    </span>
+                    <span>{roadmap.title}</span>
+                  </CardTitle>
+                  <CardDescription className="text-base text-unit-grey-40">
+                    {roadmap.location}
+                  </CardDescription>
+                </CardHeader>
+              </Card>
+            </div>
+          </div>
+        ))}
+      </div>
     </section>
   );
 }
---
+++ src/constant/Roadmap.ts
@@ -0,0 +1,59 @@
+type Roadmap = {
+  title: string;
+  date: string;
+  month_year: string;
+  location: string;
+  image: string;
+};
+
+export const roadmaps: Roadmap[] = [
+  {
+    title: "$PARIS token Launch",
+    date: "12th - 13th",
+    month_year: "April, 2024",
+    location: "Europe - Paris, France",
+    image: "/images/paris.jpg",
+  },
+  {
+    title: "$LONDON token Launch",
+    date: "26th - 27th",
+    month_year: "April, 2024",
+    location: "Europe - London, England",
+    image: "/images/london.jpg",
+  },
+  {
+    title: "$CAIRO token Launch",
+    date: "10th - 11th",
+    month_year: "May, 2024",
+    location: "Africa - Cairo, Egypt",
+    image: "/images/cairo.jpg",
+  },
+  {
+    title: "$ACCRA token Launch",
+    date: "24th - 25th",
+    month_year: "May, 2024",
+    location: "Africa - Accra, Ghana",
+    image: "/images/accra.jpg",
+  },
+  {
+    title: "$MUMBAI token Launch",
+    date: "10th - 11th",
+    month_year: "June, 2024",
+    location: "Asia - Mumbai, India",
+    image: "/images/mumbai.jpg",
+  },
+  {
+    title: "$ISTANBUL token Launch",
+    date: "24th - 25th",
+    month_year: "June, 2024",
+    location: "Europe - Istanbul, Turkey",
+    image: "/images/istanbul.jpg",
+  },
+  {
+    title: "$NEW YORK token Launch",
+    date: "8th - 9th",
+    month_year: "July, 2024",
+    location: "North America - New York, USA",
+    image: "/images/newyork.jpg",
+  },
+];
