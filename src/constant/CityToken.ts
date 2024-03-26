63179e018e83bf73008ffaf7f23fb38852a9204f
shrall-unit <marshall.kurniawan@unitcode.org>
2024-03-26 10:16:54 +0700

    feat: update CityToken type

--- src/constant/CityToken.ts
+++ src/constant/CityToken.ts
@@ -1,75 +1,129 @@
 type CityToken = {
   name: string;
   image: string;
+  link: string;
+  headline: string;
+  schedule: string;
 };
 
 export const cityTokens: CityToken[] = [
   {
     name: "London",
     image: "/images/angels.png",
+    link: "/",
+    headline: "$LONDON Token from Unit Network",
+    schedule: "Fri, Apr 26 - Apr 28, 11.00 PM GMT +1",
   },
   {
     name: "New York",
     image: "/images/angels.png",
+    link: "/",
+    headline: "$LONDON Token from Unit Network",
+    schedule: "Fri, Apr 26 - Apr 28, 11.00 PM GMT +1",
   },
   {
     name: "Istanbul",
     image: "/images/angels.png",
+    link: "/",
+    headline: "$LONDON Token from Unit Network",
+    schedule: "Fri, Apr 26 - Apr 28, 11.00 PM GMT +1",
   },
   {
     name: "Miami",
     image: "/images/angels.png",
+    link: "/",
+    headline: "$LONDON Token from Unit Network",
+    schedule: "Fri, Apr 26 - Apr 28, 11.00 PM GMT +1",
   },
   {
     name: "Berlin",
     image: "/images/angels.png",
+    link: "/",
+    headline: "$LONDON Token from Unit Network",
+    schedule: "Fri, Apr 26 - Apr 28, 11.00 PM GMT +1",
   },
   {
     name: "Cairo",
     image: "/images/angels.png",
+    link: "/",
+    headline: "$LONDON Token from Unit Network",
+    schedule: "Fri, Apr 26 - Apr 28, 11.00 PM GMT +1",
   },
   {
     name: "Paris",
     image: "/images/angels.png",
+    link: "/",
+    headline: "$LONDON Token from Unit Network",
+    schedule: "Fri, Apr 26 - Apr 28, 11.00 PM GMT +1",
   },
   {
     name: "Panama",
     image: "/images/angels.png",
+    link: "/",
+    headline: "$LONDON Token from Unit Network",
+    schedule: "Fri, Apr 26 - Apr 28, 11.00 PM GMT +1",
   },
   {
     name: "Lisbon",
     image: "/images/angels.png",
+    link: "/",
+    headline: "$LONDON Token from Unit Network",
+    schedule: "Fri, Apr 26 - Apr 28, 11.00 PM GMT +1",
   },
   {
     name: "Cape Town",
     image: "/images/angels.png",
+    link: "/",
+    headline: "$LONDON Token from Unit Network",
+    schedule: "Fri, Apr 26 - Apr 28, 11.00 PM GMT +1",
   },
   {
     name: "Nairobi",
     image: "/images/angels.png",
+    link: "/",
+    headline: "$LONDON Token from Unit Network",
+    schedule: "Fri, Apr 26 - Apr 28, 11.00 PM GMT +1",
   },
   {
     name: "San Francisco",
     image: "/images/angels.png",
+    link: "/",
+    headline: "$LONDON Token from Unit Network",
+    schedule: "Fri, Apr 26 - Apr 28, 11.00 PM GMT +1",
   },
   {
     name: "Davos",
     image: "/images/angels.png",
+    link: "/",
+    headline: "$LONDON Token from Unit Network",
+    schedule: "Fri, Apr 26 - Apr 28, 11.00 PM GMT +1",
   },
   {
     name: "Los Angeles",
     image: "/images/angels.png",
+    link: "/",
+    headline: "$LONDON Token from Unit Network",
+    schedule: "Fri, Apr 26 - Apr 28, 11.00 PM GMT +1",
   },
   {
     name: "Mexico City",
     image: "/images/angels.png",
+    link: "/",
+    headline: "$LONDON Token from Unit Network",
+    schedule: "Fri, Apr 26 - Apr 28, 11.00 PM GMT +1",
   },
   {
     name: "Seoul",
     image: "/images/angels.png",
+    link: "/",
+    headline: "$LONDON Token from Unit Network",
+    schedule: "Fri, Apr 26 - Apr 28, 11.00 PM GMT +1",
   },
   {
     name: "Tokyo",
     image: "/images/angels.png",
+    link: "/",
+    headline: "$LONDON Token from Unit Network",
+    schedule: "Fri, Apr 26 - Apr 28, 11.00 PM GMT +1",
   },
 ];
